cd2b8ee39f54a05ebd7ec65c1152540fd097ad2d
jgdelgado17 <jegadele@hotmail.com>
2023-03-13 17:35:13 -0500

    Generate Driven Adapter jefesucursal

--- lib/infrastructure/driven_adapters/repository/jefesucursal/data/jefesucursal_data.ex
+++ lib/infrastructure/driven_adapters/repository/jefesucursal/data/jefesucursal_data.ex
@@ -1,12 +1,22 @@
 defmodule EmpleadosExAlbc.Infrastructure.Adapters.Repository.Jefesucursal.Data.JefesucursalData do
   use Ecto.Schema
+  import Ecto.Changeset
 
-  ## TODO: Add schema definition
-  # Types https://hexdocs.pm/ecto/Ecto.Schema.html#module-primitive-types
-
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "jefesucursal" do
-    field :name, :string
-    #field :creation_date, :utc_datetime
-    #field :update_date, :utc_datetime
+    field :apellidos, :string
+    field :fecha_ingreso, :date
+    field :nombres, :string
+    # has_many :supervisor, EmpleadossPhoenix.Supervisores.Supervisor
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(jefesucursal, attrs) do
+    jefesucursal
+    |> cast(attrs, [:nombres, :apellidos, :fecha_ingreso])
+    |> validate_required([:nombres, :apellidos, :fecha_ingreso])
   end
 end
--- lib/infrastructure/driven_adapters/repository/jefesucursal/jefesucursal_data_repository.ex
+++ lib/infrastructure/driven_adapters/repository/jefesucursal/jefesucursal_data_repository.ex
@@ -1,28 +1,40 @@
 defmodule EmpleadosExAlbc.Infrastructure.Adapters.Repository.Jefesucursal.JefesucursalDataRepository do
   alias EmpleadosExAlbc.Infrastructure.Adapters.Repository.Repo
   alias EmpleadosExAlbc.Infrastructure.Adapters.Repository.Jefesucursal.Data.JefesucursalData
-  # alias EmpleadosExAlbc.Domain.Model.Jefesucursal
+  alias EmpleadosExAlbc.Domain.Model.Jefesucursal
 
   ## TODO: Update behaviour
-  # @behaviour EmpleadosExAlbc.Domain.Behaviours.JefesucursalBehaviour
+  @behaviour EmpleadosExAlbc.Domain.Behaviours.JefesucursalBehaviour
 
-  def find_by_id(id), do: JefesucursalData |> Repo.get(id) |> to_entity
+  def register(entity) do
+    case to_data(entity) |> Repo.insert() do
+      {:ok, entity} -> {:ok, entity |> to_entity()}
+      error -> error
+    end
+  end
+
+  def find_by_id(id) do
+    case JefesucursalData |> Repo.get!(id) do
+      {:ok, entity} -> {:ok, entity |> to_entity()}
+      error -> error
+    end
+  end
 
   def insert(entity) do
-    case to_data(entity) |> Repo.insert do
+    case to_data(entity) |> Repo.insert() do
       {:ok, entity} -> entity |> to_entity()
       error -> error
     end
   end
 
   defp to_entity(nil), do: nil
+
   defp to_entity(data) do
-    ## TODO: Update Entity
-    # struct(Jefesucursal, data |> Map.from_struct)
-    %{}
+    struct(Jefesucursal, data |> Map.from_struct())
   end
 
   defp to_data(entity) do
-    struct(JefesucursalData, entity |> Map.from_struct)
+    prop = JefesucursalData.changeset(%JefesucursalData{}, entity |> Map.from_struct()).changes
+    struct(JefesucursalData, prop)
   end
 end
