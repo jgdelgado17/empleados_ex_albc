f2c690391684ce6ebc31a44b3fe4fa725eca9583
jgdelgado17 <jegadele@hotmail.com>
2023-03-13 19:09:25 -0500

    Generate Use Case register_jefesucursal_use_case

--- lib/domain/use_cases/get_jefesucursal_use_case.ex
+++ lib/domain/use_cases/get_jefesucursal_use_case.ex
@@ -1,9 +1,19 @@
 defmodule EmpleadosExAlbc.Domain.UseCase.GetJefesucursalUseCase do
+  alias EmpleadosExAlbc.Domain.Model.Jefesucursal
+  require Logger
 
-  @moduledoc """
-  TODO Updates usecase description
-  """
-   
-  ## TODO Add functions to business logic app
+  @jefesucursal_behaviour Application.compile_env(:empleados_ex_albc, :jefesucursal_behaviour)
 
+  def find_by_id(data) do
+    with {:ok, jefesucursal} <- Jefesucursal.find_by_id(data[:id]),
+         #{:ok, _} <- legal_validation(jefesucursal),
+         {:ok, get_jefesucursal} <- find_by_id_jefesucursal(Map.get(jefesucursal, :id))  do
+           Logger.info("Founded jefesucursal: #{inspect(get_jefesucursal)}")
+         {:ok, get_jefesucursal}
+    end
+  end
+
+  def find_by_id_jefesucursal(id) do
+    @jefesucursal_behaviour.find_by_id(id)
+  end
 end
